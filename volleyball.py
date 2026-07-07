4ffd6e2a7877d9c828e055e3be44e3ac5fece844
gillianline <gillian.line@yahoo.com>
2026-07-07 10:41:54 -0400

    Update volleyball.py

--- volleyball.py
+++ volleyball.py
@@ -38,7 +38,7 @@ def check_password():
     def password_entered():
         if st.session_state["password"] == st.secrets["PASSWORD"]:
             st.session_state["password_correct"] = True
-            del st.session_state["password"]
+            st.session_state["password"]=""
         else:
             st.session_state["password_correct"] = False
             
